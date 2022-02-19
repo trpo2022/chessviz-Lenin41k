aacd6686dcdee30bbb4756b84deb34597e350bdf
PRUXA666 <rylovlsd@gmail.com>
2022-02-19 18:19:36 +0700

    create main.c, gitignore, add int=>char\char=>int functoin

---
+++ main.c
@@ -0,0 +1,58 @@
+
+
+
+
+int main() {
+
+}
+
+void print(int* cor) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+		
+		}
+	}	
+}
+
+int charint(char n) {
+	int m;
+	if (n == 'a')
+		m = 1; 
+	if (n == 'b')
+		m = 2;
+	if (n == 'c')
+		m = 3;
+	if (n == 'd')
+		m = 4;
+	if (n == 'e')
+		m = 5;
+	if (n == 'f')
+		m = 6;
+	if (n == 'g')
+		m = 7;
+	if (n == 'h')
+		m = 8;
+	return m;
+}
+
+
+char intchar(int n) {
+	char m;
+	if (n == 1)
+		m = 'a';
+	if (n == 2)
+		m = 'b';
+	if (n == 3)
+		m = 'c';
+	if (n == 4)
+		m = 'd';
+	if (n == 5)
+		m = 'e';
+	if (n == 6)
+		m = 'f';
+	if (n == 7)
+		m = 'g';
+	if (n == 8)
+		m = 'h';
+	return m; 
+}
