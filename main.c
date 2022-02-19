74ccfbd2f952cf4da6b4d3cedd6963cf83972ff5
PRUXA666 <rylovlsd@gmail.com>
2022-02-19 19:53:40 +0700

    add print function

--- main.c
+++ main.c
@@ -1,16 +1,52 @@
+#include <stdio.h>
 
-
-
+int charint(char n);
+char intchar(int n);
+void print(int* cor);
 
 int main() {
+	int cor[9][9]; 
+	
 
+
+	print(cor)
 }
 
 void print(int* cor) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-		
+			if (cor[i][j] == 10)
+				printf("K");
+			if (cor[i][j] == 11)
+				printf("k");
+			if (cor[i][j] == 20)
+				printf("Q");
+			if (cor[i][j] == 21)
+				printf("q");
+			if (cor[i][j] == 30)
+				printf("R");
+			if (cor[i][j] == 31)
+				printf("r");
+			if (cor[i][j] == 40)
+				printf("N");
+			if (cor[i][j] == 41)
+				printf("n");
+			if (cor[i][j] == 50)
+				printf("B");
+			if (cor[i][j] == 51)
+				printf("b");
+			if (cor[i][j] == 60)
+				printf("P");
+			if (cor[i][j] == 61)
+				printf("p");
+			if (cor[i][j] == 0 && j != 8 && i != 0)
+				printf(" ");
+			if (i == 0 && j != 8)
+				printf("%d", j); 
+			if (j == 8 && i != 0)
+				printf("%c", intchar(i))
 		}
+		printf("\n");
 	}	
 }
 
